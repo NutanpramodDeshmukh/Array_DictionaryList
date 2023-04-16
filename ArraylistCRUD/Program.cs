9807e6927b6cbf9cee6fbb85afb5da7d9daf555a
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-04-16 17:08:11 +0530

    Initial msg

---
+++ ArraylistCRUD/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace ArraylistCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an ArrayList
+            ArrayList student = new ArrayList();
+
+            // add elements to ArrayList
+            student.Add("Nutan");
+            student.Add("pooja");
+            student.Add("Jack");
+            // remove 
+            //student.Remove("Jack");
+            Console.WriteLine("First element: " + student[0]);
+            Console.WriteLine("Second element: " + student[1]);
+            Console.WriteLine("Third element: " + student[2]);
+
+
+            // display every element of myList 
+            // for (int i = 0; i < student.Count; i++)
+            //{
+            //Console.WriteLine(student[i]);
+            // Console.ReadKey();
+            //}
+
+            // change the value of second element 
+            student[1] = "Hrushi";
+            
+            Console.WriteLine("Updated second element: " + student[1]);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ DicCRUD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> My_dict1 = new Dictionary<int, string>();
+            My_dict1.Add(1123, "Welcome");
+            My_dict1.Add(1124, "to");
+            My_dict1.Add(1125, "home");
+
+            // foreach (KeyValuePair<int, string> ele1 in My_dict1)
+            //{
+            //Console.WriteLine("{0} and {1}",
+            //ele1.Key, ele1.Value);
+            //}
+            //Console.WriteLine();
+
+            // Using Remove() method
+            My_dict1.Remove(1123);
+            foreach (KeyValuePair<int, string> ele in My_dict1)
+            {
+                Console.WriteLine("{0} and {1}",
+                            ele.Key, ele.Value);
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
